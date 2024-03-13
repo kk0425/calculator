edcc1065f87c9663509b3f64916dbba90f89e158
kk0425 <kk0425@gmail.com>
2024-03-12 20:25:46 -0400

    fixed bugs, added more functions

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+//TODO
+//fix overflow on results; eg show 1.30000e13 or some other format
+//add keyboard support
+
 const calcButtons = document.querySelectorAll(".number-button");
 let display = document.querySelector("#display");
 let history = document.querySelector("#display-history");
 let operand1 = null;
-let operand2 = null;
 let operator = '';
 let inputReset = false;
 
 for (const button of calcButtons) {
     button.addEventListener("click", (element) => {
         if (display.textContent === "0" || inputReset) {
+            if (history.textContent.includes("=")) {
+                history.textContent = '';
+            }
             display.textContent = element.target.textContent;
             inputReset = false;
         } else if (display.textContent.length <= 9) {
@@ -20,7 +26,6 @@ for (const button of calcButtons) {
 document.querySelector("#clear").addEventListener("click", () => {
     display.textContent = 0;
     operand1 = null;
-    operand2 = null;
     operator = '';
     history.textContent = '';
     inputReset = false;
@@ -43,19 +48,53 @@ function equals() {
 }
 
 document.querySelector("#equal").addEventListener("click", () => {
-    display.textContent = equals();
+    if (operator) {
+        if (!history.textContent.includes("=")) {
+            history.textContent = operand1 + " " + operator + " " + display.textContent + " =";
+            display.textContent = equals();
+            operand1 = null;
+            operator = '';
+            inputReset = true;
+        }
+    }
 });
 
 const operatorElements = document.querySelectorAll(".operator");
 for (const operatorValue of operatorElements) {
     operatorValue.addEventListener("click", () => {
-        operator = operatorValue.textContent;
-        history.textContent = history.textContent + " " + display.textContent + " " + operator + " ";
+        if (operator) {
+            display.textContent = equals();
+        }
         operand1 = display.textContent;
+        operator = operatorValue.textContent;
+        history.textContent = display.textContent + " " + operator;
         inputReset = true;
     });
 };
 
 document.querySelector("#percent").addEventListener("click", () => {
     display.textContent = Number(display.textContent) / 100;
+});
+
+document.querySelector("#sign").addEventListener("click", () => {
+    display.textContent = Number(display.textContent) * -1;
+});
+
+document.querySelector("#backspace").addEventListener("click", () => {
+    display.textContent = Number(display.textContent.toString().slice(0, -1));
+});
+
+let memory = null;
+document.querySelector("#memory-store").addEventListener("click", () => {
+    memory = display.textContent;
+    document.querySelector("#memory-display").classList.remove("memory-display-hidden");
+});
+
+document.querySelector("#memory-recall").addEventListener("click", () => {
+    if (memory) display.textContent = memory;
+});
+
+document.querySelector("#memory-clear").addEventListener("click", () => {
+    memory = null;
+    document.querySelector("#memory-display").classList.add("memory-display-hidden");
 });
